ad9648c75c8cd71cdc505e949fe8eed38b1012fc
wangmeng <wangmeng@fenbi.com>
2017-07-31 17:27:38 +0800

    init main activity & news fragment.

---
+++ app/src/main/java/meng/keddit/features/news/NewsFragment.kt
@@ -0,0 +1,31 @@
+package meng.keddit.features.news
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.news_fragment.*
+import meng.keddit.R
+import meng.keddit.commons.inflate
+
+/**
+ * Created by meng on 2017/7/31.
+ */
+class NewsFragment : Fragment() {
+
+    private val newsList by lazy {
+        news_list
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return container?.inflate(R.layout.news_fragment)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
+    }
+}
